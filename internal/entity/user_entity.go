602e697dee4329a3dabe18be3303715d61c9bc0c
sawalreverr <sawalrever23@gmail.com>
2024-05-06 23:28:45 +0700

    chore(user): fix user auto migrate error

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	ID          string `json:"id" gorm:"type:uuid;primaryKey"`
+	ID          string `json:"id" gorm:"primaryKey"`
 	Name        string `json:"name"`
 	Email       string `json:"email"`
 	PhoneNumber string `json:"phone_number"`
